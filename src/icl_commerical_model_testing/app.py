f90d2387db23f1e1518a3e335f1618c53990c78f
Lbangayan <141669722+Lbangayan@users.noreply.github.com>
2026-02-22 18:36:27 -0800

    update google gen ai

--- src/icl_commerical_model_testing/app.py
+++ src/icl_commerical_model_testing/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 import sys
 import torch
-import google.generativeai as genai
+import google.genai as genai
 from dotenv import load_dotenv
 import subprocess
 import json
